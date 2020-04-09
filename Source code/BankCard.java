0f60868e57537e37c6f7ee94aa69d409352b4634
Abdulrahman Alghaligah <asg.1999@outlook.com>
2020-04-09 16:53:14 +0300

    adding bank card attributes

--- Source code/BankCard.java
+++ Source code/BankCard.java
@@ -1,6 +1,15 @@
 public class BankCard
 {
+	String Card_Name;
+	String Card_Number;
+	String Expire_Date;
+	String Security_code;
 	
-
-	
+	public BankCard(String name,String CN,String ED,String secure)
+	{
+		this.Card_Name = name;
+		this.Card_Number = CN;
+		this.Expire_Date = ED;
+		this.Security_code = secure;
+	}
 }
