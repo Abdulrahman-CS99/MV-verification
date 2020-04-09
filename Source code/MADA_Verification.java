742206cc3fc8d4830bfd1e53c541c5540828a209
oymmusyri <oymmusyri@sm.imamu.edu.sa>
2020-04-09 19:36:06 +0300

    Update MADA_Verification.java
    'DLL , AddingBankCard method'

--- Source code/MADA_Verification.java
+++ Source code/MADA_Verification.java
@@ -1,10 +1,16 @@
 
-
 public class MADA_Verification
 {
-	//cddd
-
-}
-
+	//BankCard CardInfo;
+	DLL<BankCard> Cards = new DLL<BankCard>(); // linked list whos hold Cards.
+	Scanner input = new Scanner(System.in);
+	
+// ----------------------------------------------- Adding bank card ---------------------------------------------------------------------------------------------------------------------------------------------------------------
+				
+	public void AddingBankCard(BankCard i)// i contains (name,CardNumber,ExpireDate,SecurityCode).
+	{
+		Cards.addLast(i);// add last to Linked List which mean: First card for mr.Omair will be first one and so on 
+		System.out.print(i.Card_Name+" Card added succsesfuly \n");
+	}
 
 
